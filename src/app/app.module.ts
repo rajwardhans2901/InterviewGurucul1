c517a0a12f785402ca391705b86075bd839e5157
rajwardhans2901 <rajwardhans2901@gmail.com>
2024-03-15 19:50:45 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './MyComponent/navbar/navbar.component';
+import { MainComponent } from './MyComponent/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
